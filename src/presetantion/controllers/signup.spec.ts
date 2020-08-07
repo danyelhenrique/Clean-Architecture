41ec9bbbcb43e5981f08df1ce6b84fd3d79a5824
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-08-07 18:29:08 -0300

    test: esure SignUpController return an error if no name is provided

--- src/presetantion/controllers/signup.spec.ts
+++ src/presetantion/controllers/signup.spec.ts
@@ -4,6 +4,7 @@ describe("SignUp Controller", () => {
 	test("Should return 400 if no name is provided", () => {
 		// System under test (sut)
 		const sut = new SignUpController()
+
 		const httpRequest = {
 			body: {
 				email: "any_name@email.com",
@@ -13,5 +14,6 @@ describe("SignUp Controller", () => {
 		}
 		const httpResponse = sut.handle(httpRequest)
 		expect(httpResponse.statusCode).toBe(400)
+		expect(httpResponse.body).toEqual(new Error("Missing param: name"))
 	})
 })

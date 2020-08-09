7b304bfa8fb6b2a3149003dcd45fcffb6e6ced9d
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-08-09 20:31:04 -0300

    feat: esnure LogControllerDecorator calss controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,17 @@
+import {
+	IController,
+	HttpRequest,
+	HttpResponse,
+} from "../../presetantion/protocols"
+
+export class LogControllerDecorator implements IController {
+	constructor(private readonly controller: IController) {}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		const httpResponse = await this.controller.handle(httpRequest)
+
+		// if (httpResponse.statusCode === 500) {
+		// }
+		return null
+	}
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,9 +3,11 @@ import { EmailValidatorAdapter } from "../../utils/emailValidatorAdapter"
 import { DbAddAccount } from "../../data/useCases/addAccount/dbAddAccount"
 import { BcryptAdater } from "../../infra/criptography/bcryptAdapter"
 import { AccountMongoRepository } from "../../infra/db/mongoDb/accountRepository/account"
+import { IController } from "../../presetantion/protocols"
+import { LogControllerDecorator } from "../decorators/log"
 
 export class MakeSignUpController {
-	static get createFactory(): SignUpController {
+	static get createFactory(): IController {
 		const emailValidatorAdapter = new EmailValidatorAdapter()
 
 		const bcryptAdater = new BcryptAdater({ salt: 12 })
@@ -22,6 +24,6 @@ export class MakeSignUpController {
 			dbAddAccount
 		)
 
-		return signUpController
+		return new LogControllerDecorator(signUpController)
 	}
 }

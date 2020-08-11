c6cbb6e109cf3f350d282bb1bf4e999de0f7af03
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-08-11 20:26:28 -0300

    chore:  change configs on lintstagedrc.json

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = (arr) => arr.join(" && ")
 
 module.exports = {
 	hooks: {
-		"pre-commit": tasks(["npm run lint-staged", "npm run test:staged"]),
+		"pre-commit": tasks(["npm run lint-staged", "yarn test:staged"]),
 		"pre-push": "npm run test:ci",
 	},
 }
